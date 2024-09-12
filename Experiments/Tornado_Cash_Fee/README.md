531403fe6b0d1a70be977464682b1126da8b15fc
Da-Wei Lee <daviddwlee84@gmail.com>
2024-09-12 16:41:39 +0800

    Able to parse transactions block by block

--- Experiments/Tornado_Cash_Fee/README.md
+++ Experiments/Tornado_Cash_Fee/README.md
@@ -11,3 +11,15 @@
   - [Ethereum | INFURA](https://docs.infura.io/api/networks/ethereum)
 - [【Ethereum 智能合約開發筆記】不用自己跑節點，使用 Infura 和 web3.js 呼叫合約.md](https://gist.github.com/Ankarrr/b33d388aa4c4411559f74d9814447ff3)
 - [How to access real-time smart contract data from Python code (using Lido contract as an example) | by Kirill Balakhonov | Chainstack | Medium](https://medium.com/@balakhonoff_47314/how-to-access-real-time-smart-contract-data-from-python-code-using-lido-as-an-example-38738ff077c5)
+- [Getting Started | Metadata API](https://metadata.etherscan.io/)
+  - [Etherscan APIs- Ethereum (ETH) API Provider](https://etherscan.io/apis)
+
+---
+
+- [Token Terminal | Fundamentals for crypto](https://tokenterminal.com/terminal/metrics/fees)
+- [Fees – Weekly fundamentals #59 | Token Terminal](https://tokenterminal.com/resources/weekly-fundamentals/59#product-tip-of-the-week)
+
+---
+
+- [Etherscan Information Center | Method ID](https://info.etherscan.com/what-is-method-id/)
+- [Ethereum Signature Database](https://www.4byte.directory/)
--- Experiments/Tornado_Cash_Fee/torn.py
+++ Experiments/Tornado_Cash_Fee/torn.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 import os
 import json
 from web3 import Web3
+from tqdm import tqdm
+from datetime import datetime
+import pandas as pd
 
 load_dotenv()
 
@@ -15,6 +18,67 @@ TORNADO_CASH_CONTRACT = os.getenv("TORN_CONTRACT_ADDRESS")
 TORNADO_ABI = json.loads(os.getenv("TORN_ABI"))
 
 contract = web3.eth.contract(address=TORNADO_CASH_CONTRACT, abi=TORNADO_ABI)
+search_blocks = 2
+end_block_number = web3.eth.get_block_number()
+
+# debug
+end_block = web3.eth.get_block(end_block_number, full_transactions=True)
+print(datetime.fromtimestamp(end_block.timestamp))
+print(f"https://etherscan.io/block/{end_block.number}")
+print(
+    f"https://etherscan.io/tx/{end_block.transactions[0].hash.hex()}",
+    "(Should match number here)",
+)
+tx_receipt = web3.eth.get_transaction_receipt(end_block.transactions[0].hash)
+print("Transaction Fee")
+print(
+    fee := tx_receipt.get("effectiveGasPrice", end_block.transactions[0].gasPrice)
+    * tx_receipt.gasUsed,
+    "Gwei",
+)
+print(web3.from_wei(fee, unit="ether"), "ETH")
+
+import ipdb
+
+ipdb.set_trace()
+
+results = []
+
+for block_num in tqdm(
+    range(end_block_number, end_block_number - search_blocks, -1),
+    desc="Blocks",
+    position=0,
+):
+    block = web3.eth.get_block(block_num, full_transactions=True)
+    timestamp = datetime.fromtimestamp(end_block.timestamp)
+    for tx in tqdm(
+        block.transactions, desc=f"Transactions of Block {block.number}", position=1
+    ):
+        if tx.to == contract.address or True:  # debug
+            tx_receipt = web3.eth.get_transaction_receipt(tx.hash)
+
+            if "effectiveGasPrice" in tx_receipt:
+                # If it's an EIP-1559 transaction, use effectiveGasPrice from the receipt
+                gas_price = tx_receipt.effectiveGasPrice
+            else:
+                # Get gasPrice from the transaction itself (tx object)
+                gas_price = tx.gasPrice  # For legacy transactions
+
+            fee = tx_receipt.gasUsed * gas_price
+
+            results.append(
+                {
+                    "time": timestamp,
+                    "block_num": block.number,
+                    "tx_hash": tx.hash.hex(),
+                    "fee": fee,
+                }
+            )
+
+result_df = pd.DataFrame(results)
+result_df["fee (ETH)"] = result_df["fee"].apply(web3.from_wei, unit="ether")
+print(result_df)
+
 
 import ipdb
 
